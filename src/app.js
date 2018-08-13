8e2e24c2a9876648e6d0a81e128247c6cbb1a50f
marcnew2101 <marcnew2101@gmail.com>
2018-08-13 08:02:10 -0600

    remove old expense data

--- src/app.js
+++ src/app.js
@@ -3,34 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.js';
 import configureStore from './store/configureStore.js';
-import { addExpense } from './actions/expenses.js';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.dispatch(addExpense({
-    description: 'FortNite',
-    note: 'Battle Pass for Switch',
-    amount: 1000,
-    createdAt: 20000
-}));
-
-store.dispatch(addExpense({
-    description: 'Gas Bill',
-    note: 'utilities',
-    amount: 6300,
-    createdAt: 21000
-}));
-
-store.dispatch(addExpense({
-    description: 'Water Bill',
-    note: 'utilities',
-    amount: 4500,
-    createdAt: 15000
-}));
-
-const state = store.getState();
 
 const jsx = (
     <Provider store={store}>
